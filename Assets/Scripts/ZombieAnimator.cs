aa3a8688a967c9cde0674544f37eaa38ca01ae69
ricardo silva <ricardo_silva_3657@hotmail.com>
2014-03-23 19:21:56 +0000

    finished part 3 of tutorial (animations)

---
+++ Assets/Scripts/ZombieAnimator.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZombieAnimator : MonoBehaviour {
+	private SpriteRenderer spriteRenderer;
+	public Sprite[] sprites;
+	public float framesPerSecond;
+
+
+	// Use this for initialization
+	void Start () {
+		spriteRenderer = renderer as SpriteRenderer;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		int index = (int)(Time.timeSinceLevelLoad * framesPerSecond);
+		index = index % sprites.Length;
+		spriteRenderer.sprite = sprites[ index ];
+	}
+}
---
+++ Assets/Scripts/ZombieController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZombieController : MonoBehaviour {
+	private Vector3 moveDirection;
+	public float moveSpeed, turnSpeed;
+
+	// Use this for initialization
+	void Start () {
+		moveDirection = Vector3.right;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 currentPosition = transform.position;
+		if(Input.GetButton("Fire1")){
+			Vector3 moveToward = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			moveDirection = moveToward-currentPosition;
+			moveDirection.z = 0;
+			moveDirection.Normalize();
+		}
+		Vector3 target = moveDirection * moveSpeed + currentPosition;
+		float targetAngle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
+
+		transform.position = Vector3.Lerp(currentPosition, target, Time.deltaTime);
+		transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0f, 0f, targetAngle), turnSpeed*Time.deltaTime);
+	}
+}
